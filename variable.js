c1a123b9a5f8c0018fc21628fa8135d82c0af4b8
LatenT-BeacOn <mayamoni930@gmail.com>
2024-09-05 13:31:38 +0600

    constant

--- variable.js
+++ variable.js
@@ -1,2 +1,6 @@
-let fistName='Maya',lastName='Mony';
+let fistName='Maya',lastName='Mony';// the value of variable can be changed;
 console.log(fistName+lastName);
+const n=7; // the value of n cannot be changed;
+console.log(n);
+// n=8;
+// console.log(n);
